59ed5822e9ca6127c54c777d3e5287b406443c4b
Anup Nayak <115527833+mrnayak25@users.noreply.github.com>
2025-06-19 04:36:42 +0000

    fixes

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {DataProvider} from './context/DataContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+<DataProvider>
     <App />
+    </DataProvider>
   </StrictMode>,
 )
--- website/vite.config.js
+++ website/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   server: {
+    host: true, // This exposes the server to the local network
+  },
   plugins: [react()],
 })
