7ccd52a8c206551b7e28b46635520bbe20d9f8d2
mrnayak25 <mrnayak27@gmail.com>
2025-07-06 16:08:57 +0530

    fixes

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -52,6 +52,7 @@ function App() {
         {/* Auth */}
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </>
   );
