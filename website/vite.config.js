85a097284f595d1151d0dc24423597069bfd8712
mrnayak25 <mrnayak27@gmail.com>
2025-07-18 23:09:44 +0530

    added my domain as a host

--- website/vite.config.js
+++ website/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   server: {
-    host: true, // This exposes the server to the local network
-      allowedHosts:'all',
+  server: {
+    host: true,
+    allowedHosts: ['career-nest-1.onrender.com', 'careernest.anupnayak.me'],
   },
   plugins: [react()],
 })
